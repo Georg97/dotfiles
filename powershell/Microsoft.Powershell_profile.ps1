5ccf7b7dfb92d2df489097abd16ec7246e97b018
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-06 23:39:24 +0200

    added config for windows-terminal and powershell

---
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -0,0 +1,52 @@
+function ConfigureNeoVim {
+    Set-Location $HOME\AppData\Local\nvim
+    nvim .
+}
+function ConfigurePowerShell {
+    Set-Location $HOME\Documents\WindowsPowerShell
+    nvim .
+}
+function ConfigureWindowsTerminal {
+    Set-Location $HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+    nvim .
+}
+function GitLogOneline {
+    git log --graph --oneline --decorate
+}
+function GitLog {
+    git log --graph --decorate
+}
+function RestartKomorebi {
+    komorebic stop; komorebic start
+}
+Set-Alias vimc ConfigureNeoVim
+Set-Alias pwshc ConfigurePowerShell
+Set-Alias termc ConfigureWindowsTerminal
+Set-Alias gitll GitLogOneline
+Set-Alias gitl GitLog
+Set-Alias lgit lazygit
+Set-Alias k kubectl
+# Set-Alias kn kubectl config set-context --current --namespace
+Set-Alias t talosctl
+Set-Alias restart-komorebic RestartKomorebi
+
+# $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\g.hirsch\.config\komorebi'
+$env:KUBE_EDITOR="nvim"
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# new powershell version path
+$pwsh_executable = 'C:\Program Files\PowerShell\7\pwsh.exe'
+if (Test-Path -Path $pwsh_executable) {
+    $pwsh_executable
+}
+
+
