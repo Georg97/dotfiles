32460c00cdccbd98141bfa93c3fb13d0897f7145
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-07 22:01:09 +0200

    modified nvim from-scratch remaps

--- nvim/from-scratch/lua/base/remap.lua
+++ nvim/from-scratch/lua/base/remap.lua
@@ -3,7 +3,8 @@ vim.g.maplocalleader = "\\"
 
 -- helpful commands
 -- print config
-vim.keymap.set("n", "<leader>getc", ":echo stdpath('config')")
+vim.keymap.set("n", "<leader>getc", ":echo stdpath('config')<cr>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 
 -- archaic telescope
 -- vim.keymap.set("n", "<leader><leader>", ":vnew | r!find . -regex .+.+<Left><Left>")
