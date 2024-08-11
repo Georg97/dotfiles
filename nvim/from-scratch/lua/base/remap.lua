0236ed3e5604e20d18a0398e6c92f8312aeb3c05
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-11 15:07:06 +0200

    updated lazy and remap in nvim from-scratch

--- nvim/from-scratch/lua/base/remap.lua
+++ nvim/from-scratch/lua/base/remap.lua
@@ -11,3 +11,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 -- vim.keymap.set("n", "<leader>o", "yy:<C-u>e <C-r>\"", {noremap = true})
 
 vim.keymap.set("n", "<leader>%", "ggVG")
+vim.keymap.set("n", "-", "@z")
