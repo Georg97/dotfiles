fbebebb4234191de885dd0ead6f1a2c40fc36934
georg <75206704+Georg97@users.noreply.github.com>
2024-08-14 23:18:36 +0200

    implemented alacritty/init.ps1 and added it to root init.ps1

--- alacritty/init.ps1
+++ alacritty/init.ps1
@@ -1,1 +1,2 @@
-echo "Windows Config for Alacritty is not implemented yet"
+mkdir ~\AppData\Roaming\alacritty
+powershell -Command { New-Item -Path ~\AppData\Roaming\alacritty\alacritty.toml -ItemType SymbolicLink -Target '.\alacritty.toml' }
--- init.ps1
+++ init.ps1
@@ -7,6 +7,10 @@ powershell -Command { .\init.ps1 }
 Pop-Location
 
 powershell -Command { New-Item -Path $HOME\AppData\Local\nvim -ItemType SymbolicLink -Value .\nvim\from-scratch }
+
+Push-Location .\alacritty
+powershell -Command { .\init.ps1 }
+Pop-Location
 #powershell -Command { .\windows-terminal\init.ps1 }
 #powershell -Command { .\alacritty\init.ps1 }
 #powershell -Command { .\helix\init.ps1 }
