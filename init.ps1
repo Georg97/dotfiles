c9b80474f53326dc821d4b99bee3a9c84d3a21b3
Georg Hirsch <G.Hirsch@imrecycling.de>
2024-08-14 19:50:08 +0200

    added top level init.ps1 to make installation on windows easier

---
+++ init.ps1
@@ -0,0 +1,6 @@
+.\powershell\init.ps1
+.\komorebi\init.ps1
+New-Item -Path $HOME\AppData\Local\nvim -ItemType SymbolicLink -Value .\nvim\from-scratch
+.\windows-terminal\init.ps1
+.\alacritty\init.ps1
+.\helix\init.ps1
