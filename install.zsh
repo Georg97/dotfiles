fbe0bb7f1fa724e95916cc959850c6ce58f2fa16
Georg <75206704+Georg97@users.noreply.github.com>
2024-08-16 14:26:44 +0200

    added lazygit to install.zsh

--- install.zsh
+++ install.zsh
@@ -2,3 +2,4 @@ ln -s $(pwd)/nvim/from-scratch ~/.config/nvim
 ln -s $(pwd)/hypr ~/.config/hypr
 ln -s $(pwd)/wofi ~/.config/wofi
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
+ln -s $(pwd)/lazygit ~/.config/lazygit
