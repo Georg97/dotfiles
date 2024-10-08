04c124f85827249496c120c31fe574976acc5415
Georg <75206704+Georg97@users.noreply.github.com>
2024-10-08 12:09:55 +0200

    added stuff for mac (yabai, skhd, intall_mac.zsh)

---
+++ install_mac.zsh
@@ -0,0 +1,5 @@
+ln -s $(pwd)/nvim/from-scratch ~/.config/nvim
+ln -s $(pwd)/git/.gitconfig ~/.gitconfig
+ln -s $(pwd)/lazygit ~/.config/lazygit
+ln -s $(pwd)/yabai/yabairc ~/.yabairc
+ln -s $(pwd)/skhd/skhdrc ~/.skhdrc
