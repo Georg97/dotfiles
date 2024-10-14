82ee96f14df1d045a61b6933f019d3ae13770db3
Georg <75206704+Georg97@users.noreply.github.com>
2024-10-14 13:23:05 +0200

    updated install.zsh

--- install.zsh
+++ install.zsh
@@ -3,3 +3,7 @@ ln -s $(pwd)/hypr ~/.config/hypr
 ln -s $(pwd)/wofi ~/.config/wofi
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
 ln -s $(pwd)/lazygit ~/.config/lazygit
+
+rm ~/.zshrc
+ln -s $(pwd)/zsh/.zshrc ~/.zshrc
+ln -s $(pwd)/zsh/themes/gtheme.zsh-theme ~/.oh-my-zsh/themes/gtheme.zsh-theme
