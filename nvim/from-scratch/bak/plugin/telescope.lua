f2f779bd97f7e8856719bb3829c8652e55c4abb6
Georg <75206704+Georg97@users.noreply.github.com>
2024-10-17 21:20:52 +0200

    changed some plugins in nvim-from-scratch config, so, that it better works with vscode

---
+++ nvim/from-scratch/bak/plugin/telescope.lua
@@ -0,0 +1,11 @@
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader><leader>", telescope.git_files, {})
+vim.keymap.set("n", "<c-p>", telescope.find_files, {})
+vim.keymap.set("n", "<leader>/", telescope.live_grep, {})
+vim.keymap.set("n", "<leader>gs", telescope.grep_string, {})
+vim.keymap.set("n", "<leader>?", telescope.help_tags, {})
+
+-- not sure if I will need these
+vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
+vim.keymap.set("n", "<leader>cmd", telescope.commands, {})
+vim.keymap.set("n", "<leader>clr", telescope.colorscheme, {})
